6b5152edf0293c9268ab28c5cb4a3dc0f466cd3b
shuichengyy5 <3331592296@qq.com>
2025-02-28 03:29:09 +0800

    1.Update 1_main.py

--- 1_main.py
+++ 1_main.py
@@ -1,4 +1,16 @@
-#
-#
-#
+#202410111303
+#3331592296@qq.com
+#余洋
 print('Hello World')
+def calculate_jewels(jewels,stones):
+    jewel_count=0
+    for stone in stones:
+        if stone in jewels:
+            jewel_count+=1
+    return jewel_count
+
+input_jewels,input_stones=input("").split(',')
+
+result=calculate_jewels(input_jewels,input_stones)
+
+print(result)
